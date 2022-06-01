8be0b44042421ea104fc9c2f8ea91bac0497e595
DenngGe <2445885680@qq.com>
2022-06-01 15:58:14 +0800

    取消timestamp

--- src/api/index.js
+++ src/api/index.js
@@ -1,99 +1,99 @@
 import requests from "./requests";
-
+// 取消?timestamp=${Date.now()}
 // 检查登录状态
 export const reqCheckLoginState = () =>
   requests({
-    url: `/login/status?timestamp=${Date.now()}`,
+    url: `/login/status`,
     method: "get",
   });
 // 首页轮播图
 export const reqFindMusicCarousel = () =>
   requests({
-    url: `/banner?type=0&timestamp=${Date.now()}`,
+    url: `/banner?type=0`,
     method: "get",
   });
 // 登陆界面国家编码
 export const reqLoginCountryCode = () =>
   requests({
-    url: `/countries/code/list?timestamp=${Date.now()}`,
+    url: `/countries/code/list`,
     method: "get",
   });
 // 手机登陆
 export const reqPhoneLogin = (data) =>
   requests({
-    url: `/login/cellphone?timestamp=${Date.now()}`,
+    url: `/login/cellphone`,
     method: "post",
     data,
   });
 // 退出登陆
 export const reqLogout = () =>
   requests({
-    url: `/logout?timestamp=${Date.now()}`,
+    url: `/logout`,
     method: "get",
   });
 // 获取歌手分类列表
 export const reqSingerList = (data) =>
   requests({
-    url: `/artist/list?timestamp=${Date.now()}`,
+    url: `/artist/list`,
     method: "post",
     data,
   });
 // 获取歌手top50歌曲
 export const reqSingerTop50List = (data) =>
   requests({
-    url: `/artist/top/song?timestamp=${Date.now()}`,
+    url: `/artist/top/song`,
     method: "post",
     data,
   });
 // 获取歌手详情
 export const reqSingerDetail = (data) =>
   requests({
-    url: `/artist/detail?timestamp=${Date.now()}`,
+    url: `/artist/detail`,
     method: "post",
     data,
   });
 // 获取歌曲URL
 export const reqSongUrl = (data) =>
   requests({
-    url: `/song/url?timestamp=${Date.now()}`,
+    url: `/song/url`,
     method: "post",
     data,
   });
 // 获取歌手专辑
 export const reqSingerAlbum = (data) =>
   requests({
-    url: `/artist/album?timestamp=${Date.now()}`,
+    url: `/artist/album`,
     method: "post",
     data,
   });
 // 获取专辑内容
 export const reqAlbumDetail = (data) =>
   requests({
-    url: `/album?timestamp=${Date.now()}`,
+    url: `/album`,
     method: "post",
     data,
   });
 // 获取推荐歌单
 export const reqRecommendPlaylist = () =>
   requests({
-    url: `/personalized?limit=10&timestamp=${Date.now()}`,
+    url: `/personalized?limit=10`,
     method: "get",
   });
 // 获取歌曲详情
 export const reqSongDetail = (data) =>
   requests({
-    url: `/song/detail?ids=${data}&timestamp=${Date.now()}`,
+    url: `/song/detail?ids=${data}`,
     method: "get",
   });
 // 获取默认搜索关键词
 export const reqDefaultSearchKeyword = () =>
   requests({
-    url: `/search/default?timestamp=${Date.now()}`,
+    url: `/search/default`,
     method: "get",
   });
 // 获取热搜列表
 export const reqHotSearch = () =>
   requests({
-    url: `/search/hot/detail?timestamp=${Date.now()}`,
+    url: `/search/hot/detail`,
     method: "get",
   });
