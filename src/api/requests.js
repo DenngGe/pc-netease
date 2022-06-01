01a9da7d31be8bddb992ab97e7bbe2e3c5fe982b
DenngGe <2445885680@qq.com>
2022-06-01 16:21:29 +0800

    延长请求时间

--- src/api/requests.js
+++ src/api/requests.js
@@ -3,7 +3,7 @@ import store from "@/store";
 
 const requests = axios.create({
   baseURL: "https://netease-cloud-music-api-one-lilac.vercel.app",
-  timeout: 5000,
+  timeout: 30000,
 });
 
 export default requests;
