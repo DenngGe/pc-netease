346ecea9001c7c9aa8642617b5b93e1a5df93a01
DenngGe <2445885680@qq.com>
2022-06-01 16:46:48 +0800

    本地调试成功

--- src/api/requests.js
+++ src/api/requests.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import store from "@/store";
 
 const requests = axios.create({
-  baseURL: "https://netease-cloud-music-api-one-lilac.vercel.app",
   timeout: 30000,
 });
 
